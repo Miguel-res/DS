878c148f2be4ba241a623c7b146fbde6ad9669ec
Miguel-res <resciamiguel@gmail.com>
2026-08-12 12:29:33 -0300

    Add files via upload

---
+++ AULA_16_Def/Aula05082026-dict/Exercicio foda de dicionario.py
@@ -0,0 +1,57 @@
+import os
+os.system("cls")
+funcionario = dict()
+while True:
+    print("""
+0 - Sair
+1 - Inicializar o dicionario
+2 - Modificar um value
+3 - Modificar uma key
+4 - Remover uma key
+5 - Listar as keys
+6 - Listar os values
+7 - Exibir o dicionario
+""")
+
+    escolha = int(input("Escolha as opções: "))
+    if escolha >= 0 and escolha <= 8:
+        match escolha:
+            case 0:
+                print("desligando programa")
+                break
+            case 1:
+                print("criando dicionario...")
+                funcionario = {
+                    "Jonas": "pedreiro"
+                }
+                print(f"Dicionario de profições criado{funcionario}")
+            case 2:
+                
+                profissao = input("Qual a profissão: ")
+                nome = input("qual o nome do cidadão(Tem que estar no Dicionario): ")
+                funcionario[nome] = profissao
+                print(funcionario)
+                
+            case 3:
+                
+                nome = input("Qual o nome do Cidadão?: ")
+                nome_novo = input("Quem voce deseja registrar?: ")
+                funcionario[nome_novo] = funcionario.pop(nome)
+                print(funcionario)
+
+            case 4:
+
+                deletar = input("Qual você quer deletar: ")
+                del funcionario[deletar]
+                print(funcionario)
+
+            case 5:
+                for k, v in funcionario.items():
+                    print(f"keys: {k}")
+
+            case 6:
+                for k, v in funcionario.items():
+                    print(f"Values: {v}")
+            case 7:
+                for k, v in funcionario.items():
+                    print(f"Keys: {k} Values: {v}")
